f2e7e2ba805116b83f330d6e4c2464f70993bf39
Clay Tinnell <ctinnell@gaig.com>
2017-09-26 20:06:51 -0400

    Finished up the UI

--- TradeMyTesla/ViewController.swift
+++ TradeMyTesla/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var model: UISegmentedControl!
+    @IBOutlet weak var upgrades: UISegmentedControl!
+    @IBOutlet weak var mileageLabel: UILabel!
+    @IBOutlet weak var mileage: UISlider!
+    @IBOutlet weak var condition: UISegmentedControl!
+    @IBOutlet weak var valuation: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stackView.setCustomSpacing(30, after: model)
+        stackView.setCustomSpacing(30, after: upgrades)
+        stackView.setCustomSpacing(30, after: mileage)
+        stackView.setCustomSpacing(60, after: condition)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calculateValue(_ sender: Any) {
+        
+    }
+    
 }
 
